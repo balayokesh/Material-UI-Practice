51668711cc8672907a6866f09b6955c6adc4ca8f
Bala Yokesh Mani A <balayokeshmani@gmail.com>
2021-07-01 18:21:01 +0530

    Learned typography

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Material-UI components
+import { Typography } from '@material-ui/core';
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant='h1'>Hello, World!</Typography>
+        </div>
+    );
 }
 
 export default App;
